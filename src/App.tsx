27724e23ef448484ee6f3d5adfd64131dc3e6c6b
Faishal Pasa <faishal.pasa@bareksa.com>
2023-03-03 12:59:01 +0700

    chore: update game

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ let randomNo = Math.floor(Math.random() * 10) + 1
 const playBackgroundSound = () => {
   if (backgroundSound) {
     backgroundSound.volume = 1
-    backgroundSound.loop
+    backgroundSound.loop = true
     backgroundSound.currentTime = 0
     backgroundSound.play()
   }
